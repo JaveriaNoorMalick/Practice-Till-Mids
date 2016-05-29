68427d919fad27ca59ba10ba0806f30ed9a41e12
JaveriaNoorMalick <javeria.noor.malick@gmail.com>
2016-05-29 14:17:22 +0000

    Add files via upload

---
+++ BuubleSort/BuubleSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuubleSort
+{
+    class Program
+    {
+        public class FindingSortedArray
+        {
+            static int[] IntegerArray = { 99,67,-1,4,45,32,1,2,3,44,0,-44,33,100,99,44,2,44,45 };
+            int loopLimit = IntegerArray.Length - 1;
+
+            public int [] numericArray()
+            {
+                for (int j = 0; j < IntegerArray.Length - 1;j++ )
+                {
+                    for (int i = 0; i < loopLimit; i++)
+                    {
+                        int temp = 0;
+                        if (IntegerArray[i] > IntegerArray[i + 1])
+                        {
+                            temp = IntegerArray[i + 1];
+                            IntegerArray[i + 1] = IntegerArray[i];
+                            IntegerArray[i] = temp;
+                        }
+                    }//end of Inner loop
+                loopLimit--;
+                 }
+                
+                return IntegerArray;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int []sortedArray;
+            FindingSortedArray obj = new FindingSortedArray();
+            sortedArray = obj.numericArray();
+            for (int i = 0; i < sortedArray.Length;i++ )
+            {
+                Console.WriteLine(sortedArray[i]);
+            }
+
+        }
+
+    }
+}
---
+++ Comparing Arrays/Comparing Arrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparing_Arrays
+{
+    class Program
+    {
+        class ComparingTwoArrays
+        {
+            
+            int[] array1={1,2,3,4,5};
+            int[] array2={2,3,1,0,5};
+        
+            public int comparer()
+            {
+                int length=array1.Length;
+                int count = 0;
+                int[] missingElements=new int [length];
+                for (int i = 0; i < array1.Length; i++ )
+                {
+                    for (int j = 0; j < array2.Length; j++ )
+                    {
+                        
+                        if (array1[i] == array2[j])
+                        {
+                            missingElements[count] = array1[i];
+                            count++;
+
+                        }
+                    }//end of nested for
+                
+                }
+                for(int k=0; k<missingElements.Length;k++)
+                {
+                    
+                    Console.WriteLine("Missing elements are : " +missingElements[k]);
+                    
+                    
+                }
+                return 99;
+            }
+        
+        }
+        static void Main(string[] args)
+        {
+            ComparingTwoArrays obj = new ComparingTwoArrays();
+            obj.comparer();
+
+        }
+    }
+}
