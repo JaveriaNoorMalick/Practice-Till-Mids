2db26a982e0a21a2285c6e2f9fc1b725703bb0d9
JaveriaNoorMalick <javeria.noor.malick@gmail.com>
2016-05-29 14:22:41 +0000

    Add files via upload

---
+++ IsNumberPowerOf2/IsNumberPowerOf2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNumberPowerOf2
+{
+    class Program
+    {
+        class TestNo
+        {
+            public bool isNoPowerOfTwo(int n)
+            {
+                if (n % 2 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            TestNo obj = new TestNo();
+            Console.WriteLine( obj.isNoPowerOfTwo(99));
+        }
+    }
+}
