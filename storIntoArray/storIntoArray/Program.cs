e9749eb50a5f1fadb1c0bee8ae80a9d2a262d5b1
JaveriaNoorMalick <javeria.noor.malick@gmail.com>
2016-05-29 14:23:14 +0000

    Add files via upload

---
+++ storIntoArray/storIntoArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingPairsWhoseSumIsGivenNo
+{
+    class Program
+    {
+        class StoreInToArray
+        {
+            static int size;
+            public void SttoreInArray()
+            {
+                string ssize = Console.ReadLine();
+                size = int.Parse(ssize);
+                
+                 int[] myArray = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    myArray[i] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+           StoreInToArray ob = new StoreInToArray();
+            ob.SttoreInArray();
+        }
+    }
+}
