50139ea75a7cf07acbff44b2180069424104fbc5
JaveriaNoorMalick <javeria.noor.malick@gmail.com>
2016-05-29 14:28:59 +0000

    Add files via upload

---
+++ palindromExample/penoldromExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace penoldromExample
+{
+    class Program
+    {
+        class IsPenoldrom
+        {
+            string strCheck;
+         
+            
+
+            public bool isPenoldrome()
+            {
+                Console.WriteLine("enter size of array");
+                int n = Console.Read();
+                Console.WriteLine("enter a string to be checked");
+                strCheck = Console.ReadLine();
+
+               char[] ary=strCheck.ToCharArray();
+                for (int i = n; i > 0; i -- )
+                {
+                    char []reverseAry;
+                    for (int j = 0; j <= n; j++)
+                    {
+                        reverseAry[i] = ary[j];
+                    }
+                }
+
+
+
+
+                return true;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
