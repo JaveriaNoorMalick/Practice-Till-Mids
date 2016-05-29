9d7ce6dff03db6f9c52c9befb0a87587b1f11d1a
JaveriaNoorMalick <javeria.noor.malick@gmail.com>
2016-05-29 14:18:50 +0000

    Add files via upload

---
+++ sring operations/sring operations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sring_operations
+{
+    class Program
+    {
+        public class strings
+        {
+            public void byRef(ref int i)
+            {
+                Console.WriteLine(i);
+                ++i;
+            }
+            public void byValue(int j)
+            {
+
+                ++j;
+            }
+            public void byOut(out int i)
+            {
+
+                i = 100;
+            }
+
+            public void checkString()
+            {
+                int i = 1;
+                byValue(i);
+                 Console.WriteLine(i);
+                 byOut(out i);
+                 Console.WriteLine(i);
+                byRef(ref i);
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            strings ob = new strings();
+            ob.checkString();
+
+
+        }
+    }
+}
+
