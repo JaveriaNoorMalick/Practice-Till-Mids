7722b5d5dd4e02014615cae9333b122706db29ab
JaveriaNoorMalick <javeria.noor.malick@gmail.com>
2016-05-29 14:19:57 +0000

    Add files via upload

---
+++ SUM WITHOUT OPERATOR/SUM WITHOUT OPERATOR/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUM_WITHOUT_OPERATOR
+{
+    class Program
+    {
+        class SumWithNoOpeator
+        {
+            static int a = 3;
+            static int b = 4;
+            int i = 0;
+            string str=a.ToString();
+            string str2 = b.ToString();
+
+            public void sum()
+            {
+               char []ch1= str.ToCharArray();
+               while (b != 0)
+               {
+                   int count = 0;
+                   char temp = ch1[count];
+                   count++;
+                
+                         b--;
+
+                         Console.WriteLine(+temp);
+                }
+
+               
+               int sum = Convert.ToInt32(ch1[i]);
+               Console.WriteLine(+sum);
+
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            SumWithNoOpeator ob = new SumWithNoOpeator();
+            ob.sum();
+            
+            
+        }
+    }
+}
