d7f6e3155d2eb1d12be9d5fe6def2d366000d9cd
JaveriaNoorMalick <javeria.noor.malick@gmail.com>
2016-05-29 14:21:32 +0000

    Add files via upload

---
+++ isBinary/isBinary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isBinary
+{
+    class Program
+    {
+        class NoIsBinary
+        {
+            string str;
+            char[] ch;
+            public bool isNumberBinary(int n)
+            {
+                str = n.ToString();
+                ch = str.ToCharArray();
+                for (int i = 0; i < ch.Length; i++)
+                {
+                    int x = (int)Char.GetNumericValue(ch[i]);
+                    Console.WriteLine(+ x);
+                    if (x == 1)
+                    {
+                        ch[i] = ch[i];
+                    }
+                    else if (x== 0)
+                    {
+                        ch[i] = ch[i];
+                    }
+                    else
+                    {
+
+                        return false;
+                    }
+                }
+                return true;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            NoIsBinary ob = new NoIsBinary();
+            Console.WriteLine( ob.isNumberBinary(111110));
+        }
+    }
+}
